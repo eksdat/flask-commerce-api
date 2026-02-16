9d139e2cfbc6388b3f55ad10defa33c1a15630f8
Pedro Aureliano <84044726+eksdat@users.noreply.github.com>
2026-02-16 16:24:57 -0300

    add product model and API endpoint for adding products

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///commerce.db'
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    return jsonify(data)
+
 
 #Definir rota e função
 @app.route('/')
